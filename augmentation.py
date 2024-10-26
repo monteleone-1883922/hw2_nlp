23c8a3f81433a7c4f85cbe7f8c47fd4ed2518723
alex <monteleone.1883922@studenti.uniroma1.it>
2024-10-26 17:20:53 +0200

    removed multithreading, lowered prob of augmenting using tautology or impossibility

--- augmentation.py
+++ augmentation.py
@@ -40,13 +40,14 @@ def choose_manipulation(sample, proportions: list):
         manipulations_list += PRODUCE_NEUTRAL_LIST
         proportions[2] += 1
         manipulation_output = 'NEUTRAL'
-
+    use_tautology_impossibility = random.random()
     for manipulation in manipulations_list:
         if not (label != 'ENTAILMENT' and (
                 manipulation == Manipulations.TRUNCATE_HYPOTHESIS or manipulation == Manipulations.CHANGE_NUMBERS)) and \
                 not ((label == manipulation_output or label == 'NEUTRAL') and manipulation == Manipulations.NEGATE_HYPOTHESIS) and \
                 not (manipulation == Manipulations.CHANGE_NUMBERS and numeric_id == -1) and \
-                not (manipulation_output == 'ENTAILMENT' and manipulation == Manipulations.CHANGE_NUMBERS and comparator == 0):
+                not (manipulation_output == 'ENTAILMENT' and manipulation == Manipulations.CHANGE_NUMBERS and comparator == 0) and \
+                not ((manipulation == Manipulations.TAUTOLOGY or manipulation == Manipulations.IMPOSSIBILITY) and use_tautology_impossibility < 0.4):
             manipulation_values.append(manipulation)
 
     if label == manipulation_output:
@@ -67,6 +68,7 @@ def isNumeric(sample):
         elif word_info['text'] in MINORITY_COMPARATORS:
             comparator = -1
     return numeric_id, comparator
+
 
 def augment_data(data: Dataset, num_new_samples: int):
     nltk.download('wordnet')
@@ -137,69 +139,6 @@ def augment_data(data: Dataset, num_new_samples: int):
     })
 
     return data, new_data_dataset, manipulation_info
-
-
-def augment_data_multithread(data : Dataset, num_new_samples: int):
-    nltk.download('wordnet')
-    premises = []
-    hypotheses = []
-    labels = []
-    new_premises = []
-    new_hypotheses = []
-    new_labels = []
-    proportions = [0, 0, 0]
-    indices = {}
-
-    # Primo for, non c'è bisogno di parallelizzarlo
-    for i, sample in enumerate(data):
-        premises.append(sample['premise'])
-        hypotheses.append(sample['hypothesis'])
-        labels.append(sample['label'])
-        indices[i] = 1
-        if sample['label'] == 'ENTAILMENT':
-            proportions[0] += 1
-        elif sample['label'] == 'CONTRADICTION':
-            proportions[1] += 1
-        else:
-            proportions[2] += 1
-
-    # Funzione da eseguire in parallelo
-    def augment_sample(i):
-        sample = data[extract_sample(indices)]  # Estrae un campione dalla lista originale
-        manipulation, output, numeric_info = choose_manipulation(sample, proportions)
-        new_sample = exec_manipulation(sample, manipulation, output, numeric_info, indices, data)
-
-        # Ritorna i valori delle colonne del nuovo campione
-        return new_sample['premise'], new_sample['hypothesis'], new_sample['label']
-
-    # Parallelizzazione del secondo for
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = executor.map(augment_sample, range(num_new_samples))
-
-    # Popola le liste new_premises, new_hypotheses, new_labels con i risultati
-    for result in results:
-        if result is not None:
-            premise, hypothesis, label = result
-            new_premises.append(premise)
-            new_hypotheses.append(hypothesis)
-            new_labels.append(label)
-
-    # Crea il dataset per i nuovi campioni
-    new_data_dataset = Dataset.from_dict({
-        'premise': new_premises,
-        'hypothesis': new_hypotheses,
-        'label': new_labels
-    })
-
-    # Crea il dataset completo concatenando i vecchi e nuovi campioni
-    data = Dataset.from_dict({
-        'premise': premises + new_premises,
-        'hypothesis': hypotheses + new_hypotheses,
-        'label': labels + new_labels
-    })
-
-    return data, new_data_dataset
-
 
 
 def print_progress_bar(percentuale: float, lunghezza_barra: int = 30, text: str="") -> None:
